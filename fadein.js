4b706317862aaf55428959ed3771d2cef7b568e8
GBalint17 <godo.balint.antal.2023i@bankitatabanya.hu>
2026-02-18 08:58:47 +0100

    Főoldal
    Felépítés és design

---
+++ fadein.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".fade-in");
+
+    elements.forEach((el, index) => {
+        setTimeout(() => {
+            el.style.opacity = "1";
+            el.style.transform = "translateY(0)";
+        }, index * 500); // Increased delay
+    });
+});
